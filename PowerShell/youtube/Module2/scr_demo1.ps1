4904c236a0c29a6b896bd2eb234319a88515bfc5
bcafferky <bryan256@msn.com>
2017-07-10 12:47:03 -0400

    add
    add

---
+++ PowerShell/youtube/Module2/scr_demo1.ps1
@@ -0,0 +1,36 @@
+﻿<#  
+Script Name:  scr_demo1.ps1
+
+Purpose:  To do cool things.
+#>
+
+Clear-Host             # Clear the console screen
+
+$myobj = "bryan"       # No type declaration = object
+$myobj
+
+[int]$myage = 33       # Strongly typed as integer
+
+$myage = "Test"
+
+$myage = "25"
+
+$myage.GetType().Name
+
+$myobj.Substring(0,3)  # Objects have methods and properties
+
+"Hi $myobj. `$myage is a nice age." # Expands variables
+
+'Hi $myobj. $myage is a nice age.'  # Does not expand vars
+
+
+<#  Above note:
+- The tilde ` is an escape that supressed variable expansion.
+- Double quotes means variables are expanded.
+- Automatically displays to the console.
+#>
+
+# Variables stay in memory.
+# Intellisense
+
+
---
+++ PowerShell/youtube/Module2/scr_demo2.ps1
@@ -0,0 +1,45 @@
+﻿# Create an array named $myArray that contains the ten numeric (int) values:1,2,3,4,5,6,7,8,9,10; 
+
+$myArray = 1,2,3,4,5,6,7,8,9,10; 
+
+[int] $sum = 0; 
+
+# Looping...
+ForEach ($val in $myArray) 
+{
+   Write-Host "Index with value: $val"; 
+   $sum = $sum + $val; 
+} 
+
+Write-Host "The sum is: $sum";
+
+# Doing comparisons...
+IF ($sum -gt 40)
+{
+  Write-Host "Wow!  That's a big number"
+}
+ELSE
+{
+    Write-Host "Ok.  Not such a big number."
+}
+
+# Doing comparisons...
+IF ($sum -gt 40 -and $myArray.Count > 6 )
+{
+  Write-Host "Greater than 40 and Count more than 6"
+}
+ELSE
+{
+    Write-Host "Condition not met"
+}
+
+
+<#  Point of confusion:
+
+    Foreach alias for ForEach-Object
+    vs.
+    ForEach statement which does not accept pipeline
+#>
+
+$myArray | ForEach-Object { "Value is $_ " }
+  
