9acf42e7877236668e170b24fb3e802bbc2c1d44
bcafferky <bryan256@msn.com>
2017-09-24 07:45:48 -0400

    del file
    del

--- Ignite2017Code/ScriptDemos/scr_demo_Module_Use.ps1
+++
@@ -1,8 +0,0 @@
-﻿#  Import our custom PowerShell Module...
-Import-Module umd_azure -Force
-
-$Myconfig = Get-udfConfig  # Returns the Configuration Object
-
-$Myconfig.emailaccount
-
-Invoke-udfAzureRMLogin -azureaccountname ($Myconfig.azureaccount) -Verbose
