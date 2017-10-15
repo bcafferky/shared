5f87cbdb71d3c527d3b763f8947118f9a35d8a16
bcafferky <bryan256@msn.com>
2017-10-15 12:45:58 -0400

    commit
    commit

---
+++ PowerShell/startjobwithcred/one_solution.ps1
@@ -0,0 +1,16 @@
+﻿<#
+   Code to run a job with credentials.
+
+   Bryan Cafferky  2017-10-15
+
+#>
+
+$init = { Import-Module umd_genericquery }
+
+$job = { GenericSqlQuery }
+
+$creds = Get-Credential 
+
+Start-Job -InitializationScript $init `
+          -ScriptBlock $job -Credential $creds | Wait-Job | Receive-Job
+          
---
+++ PowerShell/startjobwithcred/umd_genericquery/umd_genericquery.psm1
@@ -0,0 +1,18 @@
+﻿Function GenericSqlQuery
+{
+        $Datatable = New-Object System.Data.DataTable
+        $Connection = New-Object System.Data.SQLClient.SQLConnection
+        $Connection.ConnectionString = "Server=localhost;Trusted_Connection=True;Connection Timeout=30;"
+        $connection.Open()
+        $Command = New-Object System.Data.SQLClient.SQLCommand
+        $Command.Connection = $Connection
+        $Command.CommandText = "Select @@servername as name" <# for example #>
+        $DataAdapter = new-object System.Data.SqlClient.SqlDataAdapter $Command
+        $DataAdapter.SelectCommand.CommandTimeout = 600
+        $Dataset = new-object System.Data.Dataset
+        [void]$DataAdapter.Fill($Dataset)
+        return $Dataset.tables[0]
+}
+
+# GenericSqlQuery
+
